5144c6bc2f2948099d2b84e71418289ac3c0efd1
melliprasad <80943840+melliprasad@users.noreply.github.com>
2021-05-12 18:22:01 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,22 @@
+class Box {
+  constructor(x,y,width,height){
+    var options ={
+      restitution: 1,
+      isStatic: true
+    }
+
+    this.body = Bodies.rectangle(x,y,width,height, options);
+    World.add(world, this.body);
+  }
+
+  display(){
+    var pos = this.body.position;
+    rectMode(CENTER);
+    fill(255, 255, 0);
+    rect(pos.x, pos.y, this.width, this.height);
+    console.log(this.width);
+    console.log(this.height);
+    console.log(pos.x);
+    console.log(pos.y);
+  }
+}
